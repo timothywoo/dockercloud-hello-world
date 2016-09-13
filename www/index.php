6eb447fab8e5f15afb418adcd04cdb4a30516587
twoo <twoo@nycgo.com>
2016-09-13 19:51:22 -0400

    yo yo tyo

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Hello world! 2</title>
+	<title>Hello world! 1</title>
 	<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 	<style>
 	body {
